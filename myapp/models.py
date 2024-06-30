0880ab790cbea09dc122ef13c27c1843b67590d4
Tharun Karthick T <thar22107.it@rmkec.ac.in>
2024-06-30 17:21:17 +0530

    added a comment to the models file

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 # Create your models here.
+#This is a product model.
 class product(models.Model):
     def __str__(self):
         return self.name
